0623d7988b4554624ef250ad42b5f412754bed0b
Wildude <firaolder74@gmail.com>
2025-02-21 20:40:23 +0300

    Unit 4 completed (minor error fixes).

--- src/geometry/RectangleGeometry.java
+++ src/geometry/RectangleGeometry.java
@@ -17,7 +17,7 @@ public class RectangleGeometry extends Geometry {
         Vector C2 = new Vector(0f, 1f, 0f);
         Vector C3 = new Vector(0f, 0f, 1f);
         List positionList = Arrays.asList(P0, P1, P3,  P0, P3, P2)   ;
-        float[] positionData = Vector.flattenList(positionList)
+        float[] positionData = Vector.flattenList(positionList);
         List colorList = Arrays.asList(C0, C1, C3,  C0, C3, C2)   ;
         float[] colorData = Vector.flattenList(colorList);
         addAttribute("vec3", "VertexPosition", positionData);
